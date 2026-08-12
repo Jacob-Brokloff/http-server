edfc27945ac3a48f8c52def51b2356fc82494516
Jacob-Brokloff <175552884+Jacob-Brokloff@users.noreply.github.com>
2026-08-12 09:30:46 -0400

    update

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"net/http"
 	"fmt"
 	"sync/atomic"
+	"log"
+	"github.com/jacob-jbit/http-server/internal/database"
+	"os"
+	"github.com/joho/godotenv"
+	"database/sql"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
+	platform string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -41,19 +49,48 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
+	}
 	cfg.fileserverHits.Store(0)
+	err := cfg.db.Reset(r.Context())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to reset the database: " + err.Error()))
+		return
+	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0 and database reset to initial state."))
 }
 
 
 func main() {
-	apiCfg := &apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	platform := os.Getenv("PLATFORM")
+        if platform == "" {
+                log.Fatal("PLATFORM must be set")
+        }
+
+	dbQueries := database.New(db)
+	apiCfg := &apiConfig{
+		db: dbQueries,
+		platform: platform,
+	}
+
 	fileHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
